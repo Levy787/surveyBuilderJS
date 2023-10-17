cb9806a78341686064180766826f1842762090ad
Levi Quilliam <levi_quilliam@outlook.com>
2023-10-17 16:46:27 -0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'frontend/',
   server: {
     proxy: {
       "/api": "http://localhost:5000"
